d97cbea698c0a0fc9bae5d0daad063cb2ad16a53
Solveri <liamnizrad1@gmail.com>
2024-01-17 20:46:13 +0200

    Updating the player jump
    Jumop

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -24,13 +25,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      // onGround = Physics2D.CircleCast(JumpPoint.position,10,Vector2.down,2,ground);
+        // onGround = Physics2D.CircleCast(JumpPoint.position,10,Vector2.down,2,ground);
         //onGround = Physics2D.OverlapBox(JumpPoint.position,new Vector2(5,5),0,ground);
-        onGround = Physics2D.OverlapCapsule(JumpPoint.position, new Vector2(1.61f, 0.43f), CapsuleDirection2D.Horizontal, 0, ground);
+        onGround = Physics2D.BoxCast(JumpPoint.position, new Vector2(0.3511701f, 0.06263132f), 0, Vector2.down, 0.1f, ground);
     }
     private void FixedUpdate()
     {
-        if (InputManagers.instance.Movement != Vector2.zero)
+        if (InputManagers.instance.Movement != Vector2.zero && onGround)
         {
             
             Vector2 movement =  InputManagers.instance.Movement*speed*Time.deltaTime;
@@ -41,14 +42,18 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            rb.velocity = new Vector2 (0,rb.velocity.y);
+            rb.velocity = new Vector2 (rb.velocity.x,rb.velocity.y);
         }
 
         if(InputManagers.instance.CanJump && onGround)
         {
-           
-            rb.velocity = new Vector2 (rb.velocity.x, jumpForce)*Time.deltaTime;
+
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             StartCoroutine(InputManagers.instance.ResetJump());
+        }
+        if (InputManagers.instance.Movement == Vector2.zero)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
     }
 }
